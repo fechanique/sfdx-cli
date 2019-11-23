797c7c4f17355a294478f0c5a64a3c29e6709294
= <felipe.echanique@gmail.com>
2019-11-23 16:34:26 +0100

    first version

---
+++ README.md
@@ -0,0 +1,31 @@
+# Salesforce SFDX action
+
+This action allows to use the Salesforce DX CLI from GitHub Actions
+
+## Inputs
+
+### `sfdx-auth-url`
+
+**Required** Authorize a Salesforce org using an SFDX auth URL. 
+The secret must have the format "force://<refreshToken>@<instanceUrl>" or "force://<clientId>:<clientSecret>:<refreshToken>@<instanceUrl>".
+You can obtain the URL from a authorized org from your local machine using: sfdx force:org:display -u ORG-ALIAS --verbose
+
+## Example usage
+
+```yaml
+name: SFDX Test Run on Push
+
+on: [push]
+
+jobs:
+  test:
+  
+    runs-on: ubuntu-latest
+	
+    steps:
+      - uses: actions/sfdx
+        with:
+          sfdx-auth-url: ${{ secrets.AUTH_SECRET }}
+      - name: sfdx-test-run
+        run: sfdx force:apex:test:run -l RunLocalTests -w 30
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('child_process').exec;
+const fs = require('fs');
+
+try {
+  installSFDX(()=>{
+    createAuthFile(()=>{
+      authSFDX()
+    })
+  })
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+function installSFDX(next){
+  var download = 'wget https://developer.salesforce.com/media/salesforce-cli/sfdx-linux-amd64.tar.xz'
+  var createDir = 'mkdir sfdx'
+  var unzip = 'tar xJf sfdx-linux-amd64.tar.xz -C sfdx --strip-components 1'
+  var install = './sfdx/install'
+  exec(download+' && '+createDir+' && '+unzip+' && '+install, function(error, stdout, stderr){
+    if(error) throw(stderr)
+    next()
+  })
+}
+
+function createAuthFile(next){
+  fs.writeFile('./sfdx_auth.txt', core.getInput('sfdx-auth-url'), function (error, data){
+    if(error) throw(stderr)
+    next()
+  });
+}
+
+function authSFDX(){
+  exec('sfdx force:auth:sfdxurl:store -f ./sfdx_auth.txt --setdefaultusername -a SFDX-ENV', function(error, stdout, stderr){
+    if(error) throw(stderr)
+  })
+}
